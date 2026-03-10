1511933039b6ce67df6d9cc3a8607b440e8a17f8
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-03-10 14:08:31 +0700

    Initial commit

---
+++ ktp-api/src/main/java/com/tugas/ktp_api/KtpApiApplication.java
@@ -0,0 +1,13 @@
+package com.tugas.ktp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtpApiApplication.class, args);
+	}
+
+}
---
+++ ktp-api/src/test/java/com/tugas/ktp_api/KtpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.ktp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
