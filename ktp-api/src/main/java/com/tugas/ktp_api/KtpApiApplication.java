057a873b041020b3179d751456ddc62cf0872870
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-03-10 15:41:09 +0700

    add html for view page and configuration for database

--- ktp-api/src/main/java/com/tugas/ktp_api/KtpApiApplication.java
+++ ktp-api/src/main/java/com/tugas/ktp_api/KtpApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KtpApiApplication {
+public class    KtpApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KtpApiApplication.class, args);
