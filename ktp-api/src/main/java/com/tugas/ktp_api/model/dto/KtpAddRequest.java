560d73cb90783bc5a7c8aa5fe8ec66a5c48c7e86
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-03-10 15:31:58 +0700

    add KtpAddRequest DTO for handling incoming KTP payload

---
+++ ktp-api/src/main/java/com/tugas/ktp_api/model/dto/KtpAddRequest.java
@@ -0,0 +1,53 @@
+package com.tugas.ktp_api.model.dto;
+
+import java.time.LocalDate;
+
+public class KtpAddRequest {
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+
+    // TODO: Generate Getter & Setter
+
+    public String getNomorKtp() {
+        return nomorKtp;
+    }
+
+    public void setNomorKtp(String nomorKtp) {
+        this.nomorKtp = nomorKtp;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public LocalDate getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(LocalDate tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+}
